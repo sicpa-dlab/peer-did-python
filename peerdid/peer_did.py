d3e3894ea07bc749fffc2f63dbea3bc3ea085ca7
DenisRybas <denis2002.rybas@gmail.com>
2021-08-05 17:36:13 +0300

    docs updated

--- peerdid/peer_did.py
+++ peerdid/peer_did.py
@@ -22,6 +22,7 @@ def create_peer_did_numalgo_0(inception_key: PublicKeyAgreement) -> PEER_DID:
     """
     Generates peer_did according to the zero algorithm
     (https://identity.foundation/peer-did-method-spec/index.html#generation-method)
+    For this type of algorithm did_doc can be obtained from peer_did
     :param inception_key: the key that creates the DID and authenticates when exchanging it with the first peer
     :return: generated peer_did
     """
@@ -33,6 +34,7 @@ def create_peer_did_numalgo_2(encryption_keys: List[PublicKeyAgreement], signing
     """
     Generates peer_did according to the second algorithm
     (https://identity.foundation/peer-did-method-spec/index.html#generation-method)
+    For this type of algorithm did_doc can be obtained from peer_did
     :param encryption_keys: list of encryption keys
     :param signing_keys: list of signing keys
     :param service_endpoint: string conforming to the DID specification (https://www.w3.org/TR/did-core/#services)
@@ -55,6 +57,7 @@ def save_peer_did(peer_did: PEER_DID, storage: Optional[Storage] = None):
     Saves peer_did to a storage (FileStorage by default)
     :param peer_did: peer_did to save
     :param storage: repository responsible for peer_did saving
+    :raise ValueError: raises if peer_did parameter does not match peer_did spec
     """
     if is_peer_did(peer_did=peer_did):
         storage = storage or FileStorage()
