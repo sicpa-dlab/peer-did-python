333baafd6007c91350526960e4c76faf8d277094
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2021-09-28 12:28:56 +0300

    Downgrade pytest to support Python 3.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=["base58~=2.1.0", "varint~=1.0.2"],
     extras_require={
         "tests": [
-            "pytest==6.2.5",
+            "pytest==5.4.3",
         ]
     },
 )
