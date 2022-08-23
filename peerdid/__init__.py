eab86d6beed99f4d6b3ba066b13fe74b3598696f
Clément Humbert <clement.humbert@sicpa.com>
2022-08-23 08:12:34 +0200

    Fix: missing didpeer.core in wheel

--- peerdid/__init__.py
+++ peerdid/__init__.py
@@ -4,6 +4,6 @@ from . import core, dids, errors, keys
 
 from pydid import DID, DIDDocument
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 __all__ = ["__version__", "core", "errors", "dids", "keys", "DID", "DIDDocument"]
