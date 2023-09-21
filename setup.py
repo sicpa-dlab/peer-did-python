3ad95acffcadc4dc98303ed15a8f4697f9320770
Daniel Bluhm <dbluhm@pm.me>
2023-09-21 12:46:52 -0400

    chore: update pydid and drop python 3.7
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 # TODO move remaining things
 setup(
-    install_requires=["base58~=2.1.0", "pydid~=0.3.5", "varint~=1.0.2"],
+    install_requires=["base58~=2.1.0", "pydid~=0.4.0.post1", "varint~=1.0.2"],
     extras_require={"tests": ["pytest==6.2.5", "pytest-xdist==2.3.0"]},
 )
